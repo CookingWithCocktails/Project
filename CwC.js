55d7e7491e58cdf047cae11483e43e7e0efc2ec5
Charlotte Lokko <32336323+charlottelokko@users.noreply.github.com>
2017-12-12 20:46:57 +0000

    Made a submit function edit
    Added the feature so someone can't submit unless they're signed in

--- CwC.js
+++ CwC.js
@@ -44,36 +44,43 @@ window.onload = function () {
     });
 };
 
-function SubmitRecipe() {
+function SubmitRecipe(e) {
     var database = firebase.database();
     var user = firebase.auth().currentUser;
-    var author = user.displayName;
-    var title = $("#RecipeName").val();
-    var sFriendly = $("#StudentSuitable").val();
-    var method = $("#MethodInput").val();
-    var ingredients = $("#IngredientsInput").val();
-    var type = $("#SelectRecipeType").val();
-    var student =  $("#StudentSuitable").val();
-
-    var RecipeData = {
-        author: author,
-        title: title,
-        method: method,
-        ingredients: ingredients,
-        student: student,
-    };
-
-    if (type == "Food") {
-        var ref = database.ref('Recipes/Food recipes /');
-        ref.push(RecipeData);
-        alert("Success!");
+    if (user == null) {
+        alert("Please log in/register in order to submit a recipe");
+        return false;  
     }
     else {
-        var ref = database.ref('Recipes/Cocktails/');
-        ref.push(RecipeData);
-        alert("Success!");
-    }
 
+        var author = user.displayName;
+        var title = $("#RecipeName").val();
+        var sFriendly = $("#StudentSuitable").val();
+        var method = $("#MethodInput").val();
+        var ingredients = $("#IngredientsInput").val();
+        var type = $("#SelectRecipeType").val();
+        var student = $("#StudentSuitable").val();
+
+        var RecipeData = {
+            author: author,
+            title: title,
+            method: method,
+            ingredients: ingredients,
+            student: student,
+        };
+
+        if (type == "Food") {
+            var ref = database.ref('Recipes/Food recipes /');
+            ref.push(RecipeData);
+            alert("Success!");
+        }
+        else {
+            var ref = database.ref('Recipes/Cocktails/');
+            ref.push(RecipeData);
+            alert("Success!");
+        }
+        return true;  
+    }
 }
 $(document).ready(function () {
     var database = firebase.database();
@@ -92,7 +99,7 @@ $(document).ready(function () {
                     var ingredients = child.val().ingredients;
                     var method = child.val().method;
                     var title = child.val().title;
-                    thisid=child.key;
+                    thisid = child.key;
                     $('#accordion').append(
                         `<div class="card">
                         <div class="card-header" role="tab" id="heading${index}">
@@ -128,7 +135,7 @@ $(document).ready(function () {
                     var ingredients = child.val().ingredients;
                     var method = child.val().method;
                     var title = child.val().title;
-                    thisid=child.key;
+                    thisid = child.key;
                     if (student == "Yes") {
                         index = index + 1;
                         index2 = index;
@@ -147,10 +154,8 @@ $(document).ready(function () {
                                 ${ingredients}<br>
                                   <strong>  Cooking method: </strong>
                                 ${method}
-
                               </div>
                               <button class="btn buto1" id="${thisid}" onClick="reply_click(this.id)">Add to favourites</button>
-
                           </div>
                       </div>`
                         );
@@ -168,7 +173,7 @@ $(document).ready(function () {
                     var title = child.val().title;
                     index = index + 1;
                     index3 = index;
-                    thisid=child.key;
+                    thisid = child.key;
 
                     $('#accordion2').append(
                         `<div class="card">
@@ -196,20 +201,20 @@ $(document).ready(function () {
 
 
 
-        firebase.auth().onAuthStateChanged(function(user) {
-      if (user) {
-        ref4.once("value")
-            .then(function (snapshot) {
-                ref4.once("value", function (snapshot) {
-                    snapshot.forEach(function (child) {
-                      if(user.uid==child.key){
-                      child.forEach(function(childofchild){
-                        var ingredients = childofchild.val().ingredients;
-                        var method = childofchild.val().method;
-                        var title = childofchild.val().title;
-                        index = index + 1;
-                        $('#accordion432').append(
-                            `<div class="card">
+    firebase.auth().onAuthStateChanged(function (user) {
+        if (user) {
+            ref4.once("value")
+                .then(function (snapshot) {
+                    ref4.once("value", function (snapshot) {
+                        snapshot.forEach(function (child) {
+                            if (user.uid == child.key) {
+                                child.forEach(function (childofchild) {
+                                    var ingredients = childofchild.val().ingredients;
+                                    var method = childofchild.val().method;
+                                    var title = childofchild.val().title;
+                                    index = index + 1;
+                                    $('#accordion432').append(
+                                        `<div class="card">
                        <div class="card-header" role="tab" id="heading${index}">
                            <h5 class="mb-0">
                                <a data-toggle="collapse" href="#collapse${index}" aria-expanded="true" aria-controls="collapse${index}">
@@ -227,20 +232,20 @@ $(document).ready(function () {
                            <button class="btn buto1" id="${title}" onClick="reply_click3(this.id)">Delete from favourites</button>
                        </div>
                            </div>
-
                    </div>`);
-                      });
-                    }});
+                                });
+                            }
+                        });
+                    });
                 });
-            });
 
 
 
-        // User is signed in.
-      } else {
-        // No user is signed in.
+            // User is signed in.
+        } else {
+            // No user is signed in.
         }
-      });
+    });
 
 
 });
@@ -266,7 +271,7 @@ $(document).ready(function () {
                             searchResults = true;
                             index = index + 1;
 
-                            thisid=child.key;
+                            thisid = child.key;
                             $('#searchthings').append(
                                 `<div class="card">
                            <div class="card-header" role="tab" id="heading${index}">
@@ -291,7 +296,7 @@ $(document).ready(function () {
                 });
             });
 
-            ref2.once("value")
+        ref2.once("value")
             .then(function (snapshot) {
                 ref2.once("value", function (snapshot) {
                     snapshot.forEach(function (child) {
@@ -302,7 +307,7 @@ $(document).ready(function () {
                         if (title.match(lookfor) || ingredients.match(lookfor)) {
                             searchResults = true;
                             index = index + 1;
-                              thisid=child.key;
+                            thisid = child.key;
                             console.log(title);
                             $('#searchthings').append(
                                 `<div class="card">
@@ -325,7 +330,7 @@ $(document).ready(function () {
                        </div>`);
                         }
                     });
-                    if(!searchResults){
+                    if (!searchResults) {
                         $('#searchthings').html(`<h2 style="text-align: center">No results found! :'(</h2>`);
                     }
                 });
@@ -345,107 +350,105 @@ $("#searchedbutton").click(function () {
 
 
 
-function reply_click(clicked_id)
-{
-  var user = firebase.auth().currentUser;
-  if(user==null){
-    alert("Please log in/register in order to add to favourites");
-  }
-  else{
-  var database=firebase.database();
-  var ref = database.ref('Recipes/Food recipes /');
-  //pull food from the db
-  ref.once("value")
-      .then(function (snapshot) {
-          ref.once("value", function (snapshot) {
-              snapshot.forEach(function (child) {
-                if(clicked_id==child.key){
-                  var ingredients = child.val().ingredients;
-                  var method = child.val().method;
-                  var title = child.val().title;
+function reply_click(clicked_id) {
+    var user = firebase.auth().currentUser;
+    if (user == null) {
+        alert("Please log in/register in order to add to favourites");
+    }
+    else {
+        var database = firebase.database();
+        var ref = database.ref('Recipes/Food recipes /');
+        //pull food from the db
+        ref.once("value")
+            .then(function (snapshot) {
+                ref.once("value", function (snapshot) {
+                    snapshot.forEach(function (child) {
+                        if (clicked_id == child.key) {
+                            var ingredients = child.val().ingredients;
+                            var method = child.val().method;
+                            var title = child.val().title;
 
 
-                  var RecipeData = {
-                      title: title,
-                      method: method,
-                      ingredients: ingredients,
-                  };
-                      var ref = database.ref('Recipes/Favourites/'+user.uid);
-                      ref.push(RecipeData);
-                        alert("Success");
-                        location.reload();
-                }
-              });
-          });
-      });
+                            var RecipeData = {
+                                title: title,
+                                method: method,
+                                ingredients: ingredients,
+                            };
+                            var ref = database.ref('Recipes/Favourites/' + user.uid);
+                            ref.push(RecipeData);
+                            alert("Success");
+                            location.reload();
+                        }
+                    });
+                });
+            });
+    }
 }
-}
-function reply_click2(clicked_id)
-{
-  var user = firebase.auth().currentUser;
-  if(user==null){
-    alert("Please log in/register in order to add to favourites");
-  }
-  else{
-  var database=firebase.database();
-  var ref = database.ref('Recipes/Cocktails/');
-  //pull food from the db
-  ref.once("value")
-      .then(function (snapshot) {
-          ref.once("value", function (snapshot) {
-              snapshot.forEach(function (child) {
-                if(clicked_id==child.key){
-                  var ingredients = child.val().ingredients;
-                  var method = child.val().method;
-                  var title = child.val().title;
-                  var RecipeData = {
-                      title: title,
-                      method: method,
-                      ingredients: ingredients,
-                  };
-                      var ref = database.ref('Recipes/Favourites/'+user.uid);
-                      ref.push(RecipeData);
-                        alert("Success");
-                        location.reload();
-                }
-              });
-          });
-      });
-      }
+function reply_click2(clicked_id) {
+    var user = firebase.auth().currentUser;
+    if (user == null) {
+        alert("Please log in/register in order to add to favourites");
+    }
+    else {
+        var database = firebase.database();
+        var ref = database.ref('Recipes/Cocktails/');
+        //pull food from the db
+        ref.once("value")
+            .then(function (snapshot) {
+                ref.once("value", function (snapshot) {
+                    snapshot.forEach(function (child) {
+                        if (clicked_id == child.key) {
+                            var ingredients = child.val().ingredients;
+                            var method = child.val().method;
+                            var title = child.val().title;
+                            var RecipeData = {
+                                title: title,
+                                method: method,
+                                ingredients: ingredients,
+                            };
+                            var ref = database.ref('Recipes/Favourites/' + user.uid);
+                            ref.push(RecipeData);
+                            alert("Success");
+                            location.reload();
+                        }
+                    });
+                });
+            });
+    }
 }
 
-function reply_click3(clicked_id)
-{
-alert(clicked_id);
-  var database=firebase.database();
-var ref = database.ref('Recipes/Favourites/');
+function reply_click3(clicked_id) {
+    alert(clicked_id);
+    var database = firebase.database();
+    var ref = database.ref('Recipes/Favourites/');
 
-firebase.auth().onAuthStateChanged(function(user) {
-if (user) {
-ref.once("value")
-    .then(function (snapshot) {
-        ref.once("value", function (snapshot) {
-            snapshot.forEach(function (child) {
-              if(user.uid==child.key){
-              child.forEach(function(childofchild){
-                if(clicked_id==childofchild.val().title){
-              console.log(childofchild.key.toString());
-              location.reload();
-              
+    firebase.auth().onAuthStateChanged(function (user) {
+        if (user) {
+            ref.once("value")
+                .then(function (snapshot) {
+                    ref.once("value", function (snapshot) {
+                        snapshot.forEach(function (child) {
+                            if (user.uid == child.key) {
+                                child.forEach(function (childofchild) {
+                                    if (clicked_id == childofchild.val().title) {
+                                        console.log(childofchild.key.toString());
+                                        location.reload();
 
-            childofchild.ref.remove();
-            }
-              });
-            }});
-        });
+
+                                        childofchild.ref.remove();
+                                    }
+                                });
+                            }
+                        });
+                    });
+                });
+
+
+
+            // User is signed in.
+        } else {
+            // No user is signed in.
+        }
     });
-
-
-
-// User is signed in.
-} else {
-// No user is signed in.
-}
-});
 
 }
